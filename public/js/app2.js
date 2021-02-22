7f9c888a3f6a3db32e86b55e377327e0635240c4
jucastedova <100007157.joan23@fje.edu>
2021-02-22 19:51:58 +0100

    Vista crear restaurant, mapa

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,21 +1,51 @@
-// let errorChecked = document.getElementById('error--checked');
-// let submitRegister = document.getElementById('submit--control');
-// let tipoCocina = document.querySelectorAll('.filtro--tipo_cocina');
-// submitRegister.addEventListener('click', controlCheck);
+let adrecaRestaurant = document.getElementById('adreca_restaurant');
+let containerMap = document.getElementById('container-map');
+console.log('map:', containerMap);
+adrecaRestaurant.addEventListener('blur', markerMap);
 
-// function controlCheck(event) {
-//     let numberCheck = 0;
-//     for (let i = 0; i < tipoCocina.length; i++) {
-//         if (tipoCocina[i].checked) {
-//             numberCheck++;
-//             console.log('Hay seleccionados');
-//         }
-//     }
-//     console.log('Número de checks: ', numberCheck);
-//     if (numberCheck < 1) {
-//         event.preventDefault();
-//         errorChecked.innerHTML = '<p>Necessary a check in kitchen type</p>';
-//         console.log('No hay seleccionados!!');
-//     }
+var geocoder = L.esri.Geocoding.geocodeService();  
+var map = L.map('map');
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var restMarker;
+function markerMap() {
+    if (restMarker) { // Si eixsteix...
+        console.log('eliminem last control');
+        map.removeControl(restMarker); // Treiem el marker
+    }
+    containerMap.classList.remove('display-none');
+    // containerMap.classList.add('map--create-modify');
+    // console.log(adrecaRestaurant.value);
     
-// }
+
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    geocoder.geocode()
+    .address(adrecaRestaurant.value)
+    .city(`L'Hospitalet de Llobregat`)
+    .region('ES')
+    .run(function (error, response) {
+        if (error) {
+            console.log('Error', error);
+            return;
+        } else {
+            console.log('Bounds: ', response.results[0].bounds);
+            map.fitBounds(response.results[0].bounds);
+            map.setZoom(18);
+            restMarker = L.marker(response.results[0].latlng, {icon: greenIcon});
+            restMarker.addTo(map)
+                .bindPopup(`<b>${adrecaRestaurant.value}</b>`)
+                .openPopup();
+        }
+    });
+}
+
+
