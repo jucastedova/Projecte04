d6115f8e60c99caeaf227a3c21e2d58942e932a3
Xavier Jaramillo <xavijvives@gmail.com>
2021-02-26 00:05:39 +0100

    bd

--- bbdd/bd_projecte04.sql
+++ bbdd/bd_projecte04.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2021 a las 23:53:13
+-- Tiempo de generación: 26-02-2021 a las 00:04:20
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.4.14
 
